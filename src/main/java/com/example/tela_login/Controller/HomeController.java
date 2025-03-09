6bcee2ca360627f5065f5846a85eef3529c9d5c2
Mayara Martinello Bastos <maybastos2021@gmail.com>
2025-03-09 11:02:02 -0300

    feat: add configuracao de cross origin, permitido todos

--- src/main/java/com/example/tela_login/Controller/HomeController.java
+++ src/main/java/com/example/tela_login/Controller/HomeController.java
@@ -1,11 +1,13 @@
 package com.example.tela_login.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @CrossOrigin( origins = "*", allowedHeaders = "*")
     @GetMapping("/home")
     public String pagHome(){
         return "/home";
--- src/main/java/com/example/tela_login/Controller/UsuarioController.java
+++ src/main/java/com/example/tela_login/Controller/UsuarioController.java
@@ -9,10 +9,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/register")
@@ -24,6 +21,7 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
+    @CrossOrigin( origins = "*", allowedHeaders = "*")
     @PostMapping
     public ResponseEntity<UsuarioResponseDTO> registroUsuario(@RequestBody @Valid UsuarioRequestDTO dto) {
         UsuarioModel usuarioModel = usuarioService.salvarRegistro(dto);
